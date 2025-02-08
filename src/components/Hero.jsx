f517159ee0d4d0315c06849a7abb882607a38637
jameskiprop <jameskiprop01@gmail.com>
2025-02-08 23:40:48 +0300

    created the main body components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,15 +10,18 @@ const Hero = () => {
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
             <p className="font-medium text-sm md:text-base">OUR BESTSELLERS</p>
           </div>
-          <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed">
-            <div className="flex items-center gap-2">
-              <p className="font-semibold text-sm md:text-base"> SHOP NOW</p>
-              <p className="w-8 md:w-11 h-[1px] bg-[#414141]"></p>
-            </div>
+          <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">
+            {" "}
             Latest Arrivals
           </h1>
+          <div className="flex items-center gap-2">
+            <p className="font-semibold text-sm md:text-base"> SHOP NOW</p>
+            <p className="w-8 md:w-11 h-[1px] bg-[#414141]"></p>
+          </div>
         </div>
       </div>
+      {/* Hero right side */}
+      <img className="w-full sm:w-1/2 " src="/hero_img.png" alt="" />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,9 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between py-5 font-medium ">
-      <img src="/logo.png" className="w-36" alt="Logo" />
+      <Link to="/">
+        <img src="/logo.png" className="w-36" alt="Logo" />
+      </Link>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
